3e431f2d6af22860c5f02caac56a7a679c638f2a
Philippe Laban <plaban@microsoft.com>
2025-11-07 14:11:30 -0800

    beginning of the uniqueness analysis

--- populate_runs.py
+++ populate_runs.py
@@ -1,7 +1,9 @@
-from evalserv_client import EvaluationServiceClient
 from llms.genserv.client import GenerationServiceClient
-from utils_logs import get_log_counts
-import argparse, json, torch
+from evalserv_client import EvaluationServiceClient
+import argparse, json, torch, random, tqdm, time
+from concurrent.futures import ThreadPoolExecutor
+from utils_logs import get_log_counts, log_single_run
+from tasks import get_task
 
 parser = argparse.ArgumentParser()
 
@@ -9,9 +11,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--dataset_fn", type=str, default="data/sharded_instructions_600.json")
 # use the + to allow multiple model names
 parser.add_argument("--model_name", type=str, default="gs-microsoft/phi-4")
-parser.add_argument("--degree", type=int, default=100)
 parser.add_argument("--num_gpus", type=int, default=torch.cuda.device_count())
-parser.add_argument("--max_concurrent_jobs_per_worker", type=int, default=10)
+parser.add_argument("--num_runs", type=int, default=2000)
+parser.add_argument("--max_concurrent_jobs_per_worker", type=int, default=11)
 
 logs_path = f"logs/logs.jsonl"
 
@@ -30,13 +32,49 @@ with open(args.dataset_fn, "r") as f:
 
 data = [d for d in data if d["task"] == "code"]
 
-run_counts = get_log_counts(args.results_path)
+run_counts = get_log_counts(logs_path)
 
 todo_samples = []
 for sample in data:
-    print(sample.keys())
-    key = (sample["sample_id"])
-    if key not in run_counts:
-        todo_samples.append(sample)
+    key = (sample["task_id"], args.model_name)
+    num_todo_runs = args.num_runs - run_counts.get(key, 0)
+    todo_samples += [sample] * max(num_todo_runs, 0)
 
 random.shuffle(todo_samples)
+
+def populate_single_run(sample):
+    task = get_task(sample["task"])
+
+    system_message = task.generate_system_prompt(sample)
+    input_prompt = task.populate_fully_specific_prompt(sample)
+
+    conversation = [{"role": "system", "content": system_message}, {"role": "user", "content": input_prompt}]
+
+    job_schedule_result = assistant_gen_client.schedule_job(conversation, n_responses=1)
+    
+    job_status = {"status": "pending"}
+    while job_status["status"] != "completed":
+        job_status = assistant_gen_client.check_job(job_schedule_result["job_id"])
+        time.sleep(0.2)
+    
+    response = job_status["responses"][0]
+
+    this_conversation = conversation + [{"role": "assistant", "content": response["response_text"]}]
+    eval_schedule_result = eval_client.schedule_evaluation(this_conversation, sample["task"], sample)
+    eval_status = {"status": "pending"}
+    while eval_status["status"] != "completed":
+        eval_status = eval_client.check_job(eval_schedule_result["job_id"])
+        time.sleep(0.2)
+    
+    eval_result = eval_status["result"]["evaluation_return"]
+    
+    # print(f"Sample: {sample['task_id']} | Response: {response['response_text']} | Eval Result: {eval_result}")
+    log_single_run(logs_path, {"task_id": sample["task_id"], "model_name": args.model_name, "response": response["response_text"], "eval_result": eval_result})
+
+
+num_workers = args.num_gpus * args.max_concurrent_jobs_per_worker
+
+print(f">> Number of workers: {num_workers}")
+
+with ThreadPoolExecutor(max_workers=num_workers) as executor:
+    list(tqdm.tqdm(executor.map(populate_single_run, todo_samples), total=len(todo_samples), desc="Populating runs"))
--- utils_logs.py
+++ utils_logs.py
@@ -1,12 +1,19 @@
 import ujson as json
+import os
 
 def get_log_counts(logs_path):
     log_counts = {}
+    if not os.path.exists(logs_path):
+        return log_counts
     with open(logs_path, "r") as f:
         for line in f:
             log = json.loads(line)
-            key = (log["sample_id"], log["model_name"])
+            key = (log["task_id"], log["model_name"])
             if key not in log_counts:
                 log_counts[key] = 0
             log_counts[key] += 1
     return log_counts
+
+def log_single_run(logs_path, run_info):
+    with open(logs_path, "a") as f:
+        f.write(json.dumps(run_info) + "\n")
