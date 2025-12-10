a57c124c82caefbdc8dbbb67067e5da321c4bec9
Philippe Laban - sing <plaban@microsoft.com>
2025-12-10 18:58:40 -0500

    minor

--- evalserv_app.py
+++ evalserv_app.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
     
     # Get number of workers from environment variable or use default
 
-    num_workers = int(os.environ.get('EVAL_WORKERS', calculate_gpu_concurrency()["total_concurrency"]))  # Default to 8 workers to match train_mtco.py
+    num_workers = int(os.environ.get('EVAL_WORKERS', 500)) # calculate_gpu_concurrency()["total_concurrency"])
     
     # Initialize evaluation service automatically on startup
     print_colored(f"Initializing evaluation service with {num_workers} workers...", "yellow")
